c0b3235940c165189ee19f2530f908f829582ccb
Amyleep97 <168613540+Amyleep97@users.noreply.github.com>
2024-07-03 21:39:57 +1000

    Create main.py

---
+++ venv/main.py
@@ -0,0 +1,123 @@
+# Video Game Quiz
+print("Hello! Welcome to Video Game Trivia!")
+
+playing = input ("Are you ready to begin? Type Yes to start ")
+
+if playing != "Yes":
+    quit()
+
+playing = input("Question 1:")
+score = 0
+
+question = input("What year was the nintendo 64 released?")
+if question == "1996":
+    print("Correct! You're a wizard")
+    score += 1
+else:
+    print("Incorrect, sorry gamer :(")
+
+playing = input("Question 2:")
+
+question = input("What year was the nintendo 64 released?")
+if question == "1996":
+    print("Correct! Nice work!")
+    score += 1
+else:
+    print("Incorrect, Unlucky! :(")
+
+playing = input("Question 3:")
+
+
+question = input("What year was the nintendo 64 released?")
+if question == "1996":
+    print("Correct!!")
+    score += 1
+else:
+    print("Incorrect, sorry gamer :(")
+
+playing = input("Question 4:")
+
+
+question = input("What year was the nintendo 64 released?")
+if question == "1996":
+    print("Correct!!")
+    score += 1
+else:
+    print("Incorrect :(")
+
+
+playing = input("Question 5:")
+
+question = input("What year was the nintendo 64 released?")
+if question == "1996":
+    print("Correct! You're Insane!")
+    score += 1
+else:
+    print("Incorrect, that was hard hey :(")
+
+playing = input ("You're halfway! Let's go!")
+
+playing = input("Question 6:")
+
+
+question = input("What year was the nintendo 64 released?")
+if question == "1996":
+    print("Correct! You're still in this")
+    score += 1
+else:
+    print("Incorrect so close but not quite")
+
+
+playing = input("Question 7:")
+
+question = input("What year was the nintendo 64 released?")
+if question == "1996":
+    print("Correct! You're too skilled")
+    score += 1
+else:
+    print("Incorrect :(")
+
+playing = input("Question 8:")
+
+question = input("What year was the nintendo 64 released?")
+if question == "1996":
+    print("Correct! you got this win in the bag")
+    score += 1
+else:
+    print("Incorrect, unlucky! :(")
+
+playing = input("Question 9:")
+
+question = input("What year was the nintendo 64 released?")
+if question == "1996":
+    print("Correct!!!")
+    score += 1
+else:
+    print("Incorrect, maybe next time :(")
+
+playing = input ("Finish Him!") 
+
+playing = input("Question 10:")
+
+question = input("What year was the nintendo 64 released?")
+if question == "1996":
+    print("Correct! :D")
+    score += 1
+else:
+    print("Incorrect :(")
+
+print("Your score is " + str(score) + " Nice work!")
+
+playing = input ("GG, Thanks for playing!")
+
+
+
+
+
+
+    
+
+
+
+
+
